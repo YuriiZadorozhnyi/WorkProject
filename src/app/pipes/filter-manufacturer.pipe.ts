5b124c0b0e56db2c24998888cedc655381f1d02b
YuriiZadorozhnyi <yurij_zadoroznij@ukr.net>
2017-03-12 19:51:38 +0200

    Filter manufacturer pipe changed.

--- src/app/pipes/filter-manufacturer.pipe.ts
+++ src/app/pipes/filter-manufacturer.pipe.ts
@@ -8,7 +8,8 @@ export class FilterManufacturerPipe implements PipeTransform {
 
     transform(items: any[], args: any[]): any {
 
-        let argsLowerCase : any[] = [];
+        var argsLowerCase : any[] = [];
+        var count : any = 0;
 
         for (var i = 0; i < args.length; i ++) { argsLowerCase = args[i].toLowerCase(); };
 
